0326a26c10146e41eeff522ad72790289cc23984
aryan <arywnansari@gmail.com>
2022-05-07 21:45:50 +0430

    user class and userDatabase added

--- src/main/java/Controller/LoginController.java
+++ src/main/java/Controller/LoginController.java
@@ -11,6 +11,35 @@ public class LoginController {
     }
 
     public String LoginToGame(Matcher matcher) {
-        return "";
+        String first = matcher.group(5);
+        String second = matcher.group(9);
+        String username = "";
+        String password = "";
+        if (first.equals("--username") || first.equals("-u")) {
+            username = matcher.group(7);
+        }
+        else if (second.equals("--username") || second.equals("-u")) {
+            username = matcher.group(11);
+        }
+        if (first.equals("--password") || first.equals("-p")) {
+            password = matcher.group(7);
+        }
+        else if (second.equals("--password") || second.equals("-p")) {
+            password = matcher.group(11);
+        }
+        if (first.equals("") || second.equals("")) {
+            return "invalid command";
+        }
+        User user = PlayerDatabase.getPlayerDatabase().getUser(username);
+        if (user == null) {
+            return "Username and password didn’t match!";
+        }
+        if (user.getPassword().equals(password)) {
+            Menu.goToMenu(Menu.MAIN);
+            return "user logged in successfully!";
+        }
+        else {
+            return "Username and password didn’t match!";
+        }
     }
 }
---
+++ src/main/java/Controller/User.java
@@ -0,0 +1,85 @@
+package Controller;
+
+public class User {
+    private String username;
+    private String password;
+    private String nickname;
+    private int score;
+    private int gold;
+    private int turns;
+
+    public User(String username, String nickname, String password) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+        turns=0;
+        gold=0;
+        score = 0;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public int getTurns() {
+        return turns;
+    }
+
+    public void setGold(int gold) {
+        this.gold += gold;
+    }
+
+    public void setTurns(int turns) {
+        this.turns += turns;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void increaseScore(int amount) {
+        this.score += amount;
+    }
+
+    public void decreaseScore(int amount) {
+        this.score -= amount;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", score=" + score +
+                '}';
+    }
+}
