d62f1938be2135055362c692a1bc166f8655bbf9
aryan <arywnansari@gmail.com>
2022-05-09 14:58:05 +0430

    update

---
+++ src/main/java/Controller/GameController/CheatController.java
@@ -0,0 +1,11 @@
+package Controller.GameController;
+
+import java.util.regex.Matcher;
+
+public class CheatController {
+    public String increaseScience(Matcher matcher) {
+        return "";
+    }
+
+
+}
