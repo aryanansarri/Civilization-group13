17f5708d83d59c210fcd613af29540c90586b3e5
aryan <arywnansari@gmail.com>
2022-05-11 01:24:52 +0430

    remove some bad implamention

--- main/java/View/PlayGameRegex.java
+++
@@ -1,7 +0,0 @@
-package View;
-
-public class PlayGameRegex {
-    static String Exit = "(Exit Game){1}";
-    static String ShowCurrentMenu = "(show current menu){1}";
-    static String ShowMap = "(show map){1}";
-}
